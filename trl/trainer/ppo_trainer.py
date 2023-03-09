753e964507aba9a082703ed9477fd66ccce3b044
jamsluo <jamsluo@tencent.com>
2023-03-09 17:40:43 +0800

    init

--- trl/trainer/ppo_trainer.py
+++ trl/trainer/ppo_trainer.py
@@ -608,7 +608,8 @@ class PPOTrainer(BaseTrainer):
             inputs = self.tokenizer(query, return_tensors="pt")
             for key in inputs:
                 inputs[key] = inputs[key][:, :-1]
-            ge_inputs = self.tokenizer.build_inputs_for_generation(inputs, targets=response, max_gen_length=max_length+6, padding=True)
+            cur_max_length = max_length - inputs['input_ids'].shape[0]
+            ge_inputs = self.tokenizer.build_inputs_for_generation(inputs, targets=response, max_gen_length=cur_max_length, padding=True)
             ge_inputs['prompt_mask'] = inputs['attention_mask']
             print("ge_inputs", [(k, v.shape) for k, v in ge_inputs.items()])
             ge_inputs_batch.append(ge_inputs)
